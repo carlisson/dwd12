e401e60751820e663775eb03c6eff8db9243a43d
Cárlisson Galdino <cordeis@vivaldi.net>
2022-11-05 12:29:44 -0300

    Documentation for shdoc

--- dwd12.sh
+++ dwd12.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+# @file dwd12.sh
+# @brief DiceWare D12 password generator
+# @description
+# Documentation for shdoc - https://github.com/reconquest/shdoc
 
-DWD12VERSION='0.21'
+DWD12VERSION='A0.7'
+
+# Gettext configure
+source gettext.sh
+
+# @description Prints message in user language, using Gettext
+# @arg $1 string Message in English to translate
+_1text() {
+	TEXTDOMAINDIR="$_1LIB/locale" gettext 'DWD12' "$*"
+}
 
 DWD12VOLS=''
 for AUX in $HOME/.dwd12/sets /usr/local/lib/dwd12/sets /usr/lib/dwd12/sets ./sets
@@ -12,7 +25,8 @@ do
 done
 if [ "$DWD12VOLS" == "" ]
 then
-  echo "Error! No DWD12 volumes path created."
+  _1text "Error! No DWD12 volumes path created."
+  echo
   exit
 fi
 
@@ -26,7 +40,8 @@ do
 done
 if [ "$DWD12SECRETS" == "" ]
 then
-  echo "Error! No DWD12 secrets path created."
+  _1text "Error! No DWD12 secrets path created."
+  echo
   exit
 fi
 DWD12SIZE=4
@@ -35,28 +50,30 @@ _mode="normal"
 _verbose=0 #Disable
 _vfile=$(mktemp)
 
-# Print only if mode verbose is active
-# @param string to Print
-function _vprint {
-  echo "${FUNCNAME[1]} $1" >> $_vfile
+# @description Print only if mode verbose is active
+# @arg $1 string Message to print
+_vprint() {
+  echo "${FUNCNAME[1]} $*" >> $_vfile
 }
 
-# Run x d12
-# @param Number of dices. Default: 1
-function _rund12 {
+# @description Run x d12
+# @arg $1 int Number of dices. Default: 1
+_rund12() {
+  local DICES
 	if [ $# -eq 0 ]
 	then
 		DICES=1
 	else
 		DICES="$1"
 	fi
-  _vprint "Roll "$DICES"d12"
+  _vprint "$(printf "$(_1text "Roll %sd12")" "$DICES")"
 	tr -dc '123456789XJQ' < /dev/urandom | head -c "$DICES" | sed 's/\(.\)/\1\  /g' | sed 's/X/10/g' | sed 's/J/11/g' | sed 's/Q/12/g'; echo
 }
 
-# Sort a word
-# @param Total of volumes
-function _sortaword {
+# @description Sort a word
+# @arg $1 int Total of volumes
+_sortaword() {
+  local VOLS MV PLUS V2 WRD VOL MSG
 	VOLS=$_dvls
 	if [ "$#" -ge 1 ]
 	then
@@ -68,7 +85,7 @@ function _sortaword {
 			fi
 		fi
 	fi
-  _vprint "Will work with $VOLS volumes"
+  _vprint "$(printf "$(_1text "Will work with %i volumes.")" "$VOLS")"
 	MV=""
 	PLUS=""
 	if [ "$VOLS" -ge 10 ]
@@ -76,7 +93,8 @@ function _sortaword {
 		(( "V2 = $VOLS - 10" ))
 		if [ "$V2" -ge 10 ]
 		then
-			echo "It don't works for more then 19 volumes."
+			_1text "It don't works for more then 19 volumes."
+      echo
 			return 1
 		fi
 		PLUS="A"
@@ -112,14 +130,16 @@ function _sortaword {
 	if [ "$VOL" = 'H' ]; then VOL=17; fi
 	if [ "$VOL" = 'I' ]; then VOL=18; fi
 	if [ "$VOL" = 'J' ]; then VOL=19; fi
-  _vprint "Volume $VOL, $WRD"
-  echo "Volume $VOL, $WRD"
+  MSG="$(printf "$(_1text "Volume %i, %s")" $VOL "$WRD")"
+  _vprint "$MSG"
+  echo "$MSG"
 }
 
-# Sort the passphrase, just positions
-# @param Number of volumes (default: 4)
-# @param Number of words (default: 4)
-function _predwd12 {
+# @description Sort the passphrase, just positions
+# @arg $1 int Number of volumes (default: 4)
+# @arg $2 int Number of words (default: 4)
+_predwd12() {
+  local VOLS MSG
 	VOLS=$_dvls
 	if [ "$#" -ge 1 ]
 	then
@@ -133,38 +153,46 @@ function _predwd12 {
 	fi
 	for i in $(seq 1 "$DWD12SIZE")
 	do
-		echo -n "WORD $i: "
-    _vprint "$VOLS"
-		_sortaword "$VOLS"
+		MSG="$(printf "$(_1text "WORD %i: %s")" $i "$(_sortaword "$VOLS")")"
+    _vprint "$MSG"
+    echo "$MSG"
 	done
 }
 
-# Password generator
-# @param
-function rundwd12 {
+# @description Password generator
+rundwd12() {
+  local ACTPOS VOLS SECPOS j IJ
   ACTPOS=0
-  _vprint "Running with $DWD12SET set. Generating passphrase with $DWD12SIZE words."
-  if [ $_mode == "secure" ]
+  _vprint "$(printf "$(_1text "Running with %s set. Generating passphrase with %i words.")" "$DWD12SET" $DWD12SIZE)"
+
+  if [ "$_mode" == "secure" ]
   then
     VOLS=$_dvls
     SECPOS=$(shuf -i 1-$DWD12SIZE -n 1)
-    _vprint "The position $SECPOS will turn into a secret-vol word."
+    _vprint "$(printf "$(_1text "The position %i will turn into a secret-vol word.")" "$SECPOS")"
   else
     VOLS=$((_dvls + _dsec))
     SECPOS=-1
     if [ $_dsec -eq 1 ]
     then
-      _vprint "There are $_dvls volumes. Number $VOLS is the secret ($DWD12SEC)!"
+      _vprint "$(printf "$(_1text "There are %i volumes. Number %i is the secret (%s)!")" $_dvls $VOLS "$DWD12SEC")"
     fi
   fi
+
   if [ $_verbose -eq 1 ]
   then
-    echo -n "Passphrase: "
+    _1text "Passphrase: "
   fi
-	for j in $(_predwd12 "$VOLS" | sed 's/[[:alpha:]]//g' | sed 's/^ //' | sed 's/://' | sed 's/,//' | sed 's/[ ]\+/-/g')
+
+	for j in $(_predwd12 "$VOLS" | \
+      sed 's/[[:alpha:]]//g' | \
+      sed 's/^ //' | \
+      sed 's/://' | \
+      sed 's/,//' | \
+      sed 's/[ ]\+/-/g')
 	do
-		#IJ é um array a partir do resultado de sorteio de uma palavra dicewared12
-		# onde 0: contagem de palavras; 1: tomo; 2, 3, 4: palavra
+		#IJ is an array from word shuffle result from dwd12
+		# 0: word counter; 1: vol; 2, 3, 4: words
 		IJ=(${j//-/ })
     ACTPOS=$((ACTPOS + 1))
     if [ $_dsec -eq 1 ]
@@ -180,7 +208,7 @@ function rundwd12 {
     else
       JFILE=$(find "$_dset" -maxdepth 1 -type f | sed -n "${IJ[1]}p")
     fi
-    _vprint "Using a word from volume $JFILE."
+    _vprint "$(printf "$(_1text "Using a word from volume %s.")" "$JFILE")"
 		JWORD=$(( (IJ[2]-1)*144+(IJ[3]-1)*12+IJ[4] ))
 		JDW=$(sed -n "$JWORD"p < "$JFILE")
 		echo -n "$JDW "
@@ -188,87 +216,100 @@ function rundwd12 {
 	echo
 }
 
-function _dwd12set {
+# @description Choose a DWD12 set
+# @arg $1 string Set ID
+_dwd12set() {
   DWD12SET="$1"
-  _vprint "Selected set $DWD12SET"
+  _vprint "$(printf "$(_1text "Selected set %s")" "$DWD12SET")"
 }
 
-function _dwd12secret {
+# @description Choose the secret DWD12 volume
+# @arg $1 string Volume name
+_dwd12secret() {
+  local _dsec
   if [ $(find $DWD12SECRETS -mindepth 1 -maxdepth 1 -name $1.txt | head -1) != "" ]
   then
     _dsec=1
     DWD12SEC="$1"
-    _vprint "Selected secret volume $DWD12SEC"
+    _vprint "$(printf "$(_1text "Selected secret volume %s")" "$DWD12SEC")"
   else
-    echo "Error! Secret volume $1 not found."
+    printf "$(_1text "Error! Secret volume %s not found.")\n" "$1"
     rm $_vfile
     exit
   fi
 }
 
-function _dwd12words {
+# @description Set passphrasesize
+# @arg $1 int Passwphrase size
+_dwd12words() {
   if [ "$1" -gt 0 ]
   then
     if [ "$1" -lt 50 ]
     then
       DWD12SIZE="$1"
-      _vprint "Passphrase size set to $DWD12SIZE"
+      _vprint "$(printf "$(_1text "Passphrase size set to %i")" $DWD12SIZE)"
     fi
   fi
 }
 
-function _dwd12verbose {
+# @description Set verbose mode
+# @arg $1 string false or true
+_dwd12verbose() {
   _showvars "Before set verbose"
   case $1 in
     false )
       _verbose=0
-      _vprint "Mode verbose turned off"
+      _vprint "$(_1text "Mode verbose turned off")"
       ;;
     true )
       _verbose=1
-      _vprint "Mode verbose turned on"
+      _vprint "$(_1text "Mode verbose turned on")"
       ;;
   esac
   _showvars "After set verbose"
 }
 
-function _showvars {
+# @description Prints all variable and values
+_showvars() {
   if [ $_verbose -eq 2 ]
   then
     echo $1
-    echo "Variables:"
-    echo "  Set: $DWD12SET"
-    echo "  Secret: $DWD12SEC"
-    echo "  Mode: $_mode"
-    echo "  Size: $DWD12SIZE words"
-    echo "  Verbose: $_verbose"
+    printf "$(_1text "Variables:")\n"
+    printf "  $(_1text "Set: %s")\n" "$DWD12SET"
+    printf "  $(_1text "Secret: %s")\n" "$DWD12SEC"
+    printf "  $(_1text "Mode: %s")\n" "$_mode"
+    printf "  $(_1text "Size: %i words")\n" $DWD12SIZE
+    printf "  $(_1text "Verbose: %s")\n" "$_verbose"
   fi
 }
 
-# List all volumes sets available
-function showsets {
+# @description List all available volumes sets
+showsets() {
+  local s
   for s in $(find $DWD12VOLS -mindepth 1 -maxdepth 1 -type d)
   do
     echo $(basename $s) "  $s"
   done
 }
 
-# List all secret volumes available
-function showsecrets {
+# @description List all available secret volumes
+showsecrets() {
+  local s
   for s in $(find $DWD12SECRETS -maxdepth 1 -name '*.txt')
   do
     echo $(basename $s .txt) "  $s"
   done
 }
 
-# Print information about actual vomules set
-function showinfo {
+# @description Prints information about actual vomules set
+showinfo() {
+  local i
   echo "DWD12 $DWD12VERSION"
   echo $0
   echo
-  echo "Set: $DWD12SET"
-  echo "Path: $_dset"
-  echo "Volumes: $_dvls"
+  printf "$(_1text "Set: %s")\n" "$DWD12SET"
+  printf "$(_1text "Path: %s")\n" "$_dset"
+  printf "$(_1text "Volumes: %s")\n" "$_dvls"
   for i in $(find "$_dset" -maxdepth 1 -type f)
   do
     echo "  -" $(basename $i)
@@ -277,39 +318,40 @@ function showinfo {
 
   if [ $_dsec == 0 ]
   then
-    echo "Not using secret volume."
+    printf "$(_1text "Not using secret volume.")\n"
   else
-    echo "Secret volume: $DWD12SEC"
+    printf "$(_1text "Secret volume: %s")\n" "$DWD12SEC"
   fi
 
   for i in $DWD12VOLS
   do
-    echo "Sets in $i: " $(ls $i | wc -l)
+    printf "$(_1text "Sets in %i: %s.")\n" $i "$(ls $i | wc -l)"
   done
 
 }
 
-# Print help menu
-function showhelp {
+# @description Print help menu
+showhelp() {
   echo "DWD12 $DWD12VERSION"
-  echo "      Passphrase generator!"
+  echo "      $(_1text "Passphrase generator!")"
   echo
-  echo "$ dwd12 [options]"
+  echo "$ dwd12 [$(_1text "options")]"
   echo
-  echo "  -s _set   Generate passphrase using the set _set."
-  echo "  -x _svol  Use given secret volume."
-  echo "  -z _svol  Use given secret volume (exactly 1 of the words in passphrase)."
-  echo "  -w _size  Gerenate passphrase with _size words."
-  echo "  -i        Just show information about selected set."
-  echo "  -l        List all volumes sets available to use."
-  echo "  -X        List all secret volumes available."
-  echo "  -v        Enable verbose mode."
-  echo "  -h        Show this help menu"
+  echo "  -s _set   $(_1text "Generate passphrase using the set _set.")"
+  echo "  -x _svol  $(_1text "Use given secret volume.")"
+  echo "  -z _svol  $(_1text "Use given secret volume (exactly 1 of the words in passphrase).")"
+  echo "  -w _size  $(_1text "Gerenate passphrase with _size words.")"
+  echo "  -i        $(_1text "Just show information about selected set.")"
+  echo "  -l        $(_1text "List all volumes sets available to use.")"
+  echo "  -X        $(_1text "List all secret volumes available.")"
+  echo "  -v        $(_1text "Enable verbose mode.")"
+  echo "  -h        $(_1text "Show this help menu")"
   echo
 }
 
-# Read the configuration file. PENDING
-function _readconf {
+# @description Read the configuration file. PENDING
+_readconf() {
+  local FILE RCPARAM RCVALUE DWD12SET DWD12SEC _dsec
   FILE="$1"
   if [ -f "$FILE" ]
   then
@@ -325,10 +367,10 @@ function _readconf {
           case "$RCVALUE" in
             normal | secure )
               _mode=$RCVALUE
-              _vprint "Mode set to $RCVALUE"
+              _vprint "$(printf "$(_1text "Mode set to %s")" "$RCVALUE")"
               ;;
             secret ) _mode=normal ;;
-            * ) _vprint "Invalid option ($RCVALUE) to $RCPARAM."
+            * ) _vprint "$(printf "$(_1text "Invalid option (%s) to %s.")" "$RCVALUE" "$RCPARAM")"
           esac
           ;;
         WORDS ) _dwd12words "$RCVALUE" ;;
@@ -337,30 +379,33 @@ function _readconf {
           ;;
       esac
     done
-    _showvars "Inside readconf, middle."
+    _showvars "$(_1text "Inside readconf, middle.")"
   else
-    _vprint "Config file $FILE not found."
+    _vprint "$(printf "$(_1text "Config file %s not found.")" "$FILE")"
   fi
-  _showvars "Inside readconf, before go out."
+  _showvars "$(_1text "Inside readconf, before go out.")"
 }
+
+
+# MAIN CODE
 
 DWD12SET=$(showsets | head -1 | cut -d\  -f 1)
 DWD12SEC=$(basename $(find secrets -name '*.txt' | head -1) .txt)
 _dsec=0
 
-_showvars "Before configuration file"
+_showvars "$(_1text "Before configuration file")"
 
 _readconf "/etc/dwd12/dwd12.conf"
 _readconf "$HOME/.dwd12/dwd12.conf"
 
-_showvars "After configuration file"
+_showvars "$(_1text "After configuration file")"
 
 while getopts "s:x:z:w:lXivh" option
 do
   case ${option} in
     i ) #Information about the set of volumes
       _mode="info"
-      _vprint "Mode set to info"
+      _vprint "$(_1text "Mode set to info")"
       ;;
     s ) #Set of volumes
       _dwd12set "$OPTARG"
@@ -379,14 +424,14 @@ do
       _dwd12words "$OPTARG"
       ;;
     l  ) #List all sets available
-      echo "Sets available:"
+      _1text "Sets available: "
       showsets
       rm $_vfile
       exit
 
       ;;
     X  ) # List all secret volumes
-      echo "Secret volumes:"
+      _1text "Secret volumes: "
       showsecrets
       rm $_vfile
       exit
@@ -410,7 +455,7 @@ _dvls=$(find "$_dset" -maxdepth 1 -type f | wc -l)
 
 if [ "$_dset" = "" ]
 then
-  echo "Error. Set $DWD12SET not found."
+  printf "$(_1text "Error. Set %s not found.")" "$DWD12SET"
 else
   case ${_mode} in
     info)
